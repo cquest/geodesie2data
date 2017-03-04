f84d36f70bdc058afa564d50af43727d18a480da
cquest <cquest@openstreetmap.fr>
2017-03-04 15:45:42 +0100

    ajout export CSV

--- geodesie2data.sh
+++ geodesie2data.sh
@@ -1,5 +1,5 @@
-# récupération données via WFS
-ogr2ogr -f geojson rbf.json WFS:http://geodesie.ign.fr/cgi-bin/mapserv?map=/var/webapp/lib/visugeod/mapfile_bdg.map sit_rbfType
+# récupération données RBF via WFS, attention lat/lon inversés !
+ogr2ogr -f geojson rbf.json "WFS:http://geodesie.ign.fr/cgi-bin/mapserv?map=/var/webapp/lib/visugeod/mapfile_bdg.map" sit_rbfType
 
 # récupération des PDF
 mkdir -p rbf
@@ -8,12 +8,15 @@ jq -r '.features[].properties|[.url, .nom]|@csv' rbf.json | sed 's/"//g' | paral
 # extraction texte contenu dans les PDF
 for f in rbf/*.pdf; do pdf2txt.py $f > $f.txt; done
 # conversion en json
-: > rbf.sjson ;for f in rbf/*.pdf.txt; do echo $f; python3 txt2data.py $f >> rbf.sjson; done
+: > rbf.sjson ;for f in rbf/*.pdf.txt; do echo $f; python3 txt2data.py $f json >> rbf.sjson; done
 jq -s '.' rbf.sjson > rbf-all.json
 
-exit
+# conversion en csv
+echo 'numero,indice,id,nivellement,date_fiche,reseau,site_nom,site_num,commune,ld,departement,description,gps,vu,lat,lon,ele,x,y,z,precision_plani_max,precision_alti_max,ref_latlon,ref_proj,ref_proj_epsg,ref_proj_alti' > rbf-all.csv
+for f in rbf/*.pdf.txt; do echo $f; python3 txt2data.py $f csv >> rbf-all.csv; done
 
-ogr2ogr -f geojson rdf.json WFS:http://geodesie.ign.fr/cgi-bin/mapserv?map=/var/webapp/lib/visugeod/mapfile_bdg.map sit_rdfType
+# récupération données RDF via WFS, attention lat/lon inversés !
+ogr2ogr -f geojson rdf.json "WFS:http://geodesie.ign.fr/cgi-bin/mapserv?map=/var/webapp/lib/visugeod/mapfile_bdg.map" sit_rdfType
 mkdir -p rdf
 jq -r '.features[].properties|[.url, .nom]|@csv' rdf.json | sed 's/"//g' | parallel --colsep ',' echo {2} \; wget -q {1} -O rdf/{2}.pdf
 
@@ -23,6 +26,12 @@ for f in rdf/*.pdf; do pdf2txt.py $f > $f.txt ; done
 : > rdf.sjson ; for f in rdf/*.pdf.txt; do echo $f; python3 txt2data.py $f >> rdf.sjson; done
 jq -s '.' rdf.sjson > rdf-all.json
 
+# conversion en csv
+echo 'numero,indice,id,nivellement,date_fiche,reseau,site_nom,site_num,commune,ld,departement,description,gps,vu,lat,lon,ele,x,y,z,precision_plani_max,precision_alti_max,ref_latlon,ref_proj,ref_proj_epsg,ref_proj_alti' > all.csv
+for f in rbf/*.pdf.txt; do echo $f; python3 txt2data.py $f csv >> all.csv; done
+for f in rdf/*.pdf.txt; do echo $f; python3 txt2data.py $f csv >> all.csv; done
+
+exit
 
 ogr2ogr -f geojson rn.json WFS:http://geodesie.ign.fr/cgi-bin/mapserv?map=/var/webapp/lib/visugeod/mapfile_bdg.map sit_rnType
 mkdir -p rn
