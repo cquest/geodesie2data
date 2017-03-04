213190695680deff7e811e800fc24cb89db94704
cquest <cquest@openstreetmap.fr>
2017-03-04 14:45:25 +0100

    correction + sortie json/csv/debug

--- txt2data.py
+++ txt2data.py
@@ -1,5 +1,7 @@
+#!/usr/bin/python3
 import sys
 import json
+import csv
 import re
 
 state = 0
@@ -8,18 +10,28 @@ re_lonlatele = re.compile(r"^ *(\d+)° *(\d+)' ([0-9\.]+)'' ([EO]) *(\d+)° (\d+
 re_lonlat = re.compile(r"^ *(\d+)° *(\d+)' ([0-9\.]+)'' ([EO]) *(\d+)° (\d+)' ([0-9\.]*)''$")
 re_lat = re.compile(r"^ *(\d+)° (\d+)' ([0-9\.]*)'' ([NS]) *([\-0-9\.]*)$")
 re_lon = re.compile(r"^ *(\d+)° *(\d+)' ([0-9\.]+)'' ([EO])$")
-re_alti = re.compile(r" +([\-0-9\.]*)$")
+re_alti = re.compile(r" +([\-0-9\.]+)$")
 re_date= re.compile(r"^(\d\d)/(\d\d)/(\d\d\d\d)")
+
+# format de sortie json ou csv
+sortie = 'json'
+try:
+    sortie = sys.argv[2]
+except:
+    pass
+
+if sortie=='csv':
+    fieldnames = ['numero','indice','id','nivellement','date_fiche','reseau','site_nom','site_num','commune','ld','departement','description','gps','vu','lat','lon','ele','x','y','z','precision_plani_max','precision_alti_max','ref_latlon','ref_proj','ref_proj_epsg','ref_proj_alti']
+    csvout = csv.DictWriter(sys.stdout, fieldnames=fieldnames)
+    #csvout.writeheader()
 
 f = open(sys.argv[1])
 for l in f:
   l=l[:-1]
-  #print(state,l)
+  if sortie == 'debug':
+      print(state,l)
 
   if state==0 and l=='Réseau Géodésique Français': # première page, entête
-    state=1
-  elif state==1 and l!='':
-    d['site_nom'] = l
     state=2
   elif state==2 and l[:10]=='No du Site':
     d['site_num']=l[12:]
@@ -28,20 +40,24 @@ for l in f:
   elif state==2 and l[:9]=='Commune :':
     d['commune']=l[11:]
   elif state==2 and l[:10]=='Lieu-dit :':
-    d['ld']=l[12:]
+    if l[12:] != '':
+        d['ld']=l[12:]
   elif state==2 and l[:7]=='Site du':
     d['reseau']=l[9:]
   elif state==2 and l=='IGN/SGN': # page suivante, détail
     state=3
+  elif state==2 and l!='' and 'site_nom' not in d:
+    d['site_nom'] = l
   elif (state==3) and re_date.match(l) is not None:
     date = re_date.match(l)
-    d['date']=date.group(3)+'/'+date.group(2)+'/'+date.group(1)
+    d['date_fiche']=date.group(3)+'/'+date.group(2)+'/'+date.group(1)
   elif (state==2 or state==3 or state==6 or state==7) and l[:13]=='Identifiant :':
-    d['reperes'].append({})
     num = l.find('NO :')
-    d['reperes'][len(d['reperes'])-1]['id']=l[14:num-3]
-    d['reperes'][len(d['reperes'])-1]['numero']=l[num+5:]
-    state = 4
+    if num>0:
+        d['reperes'].append({})
+        d['reperes'][len(d['reperes'])-1]['id']=l[14:num-3]
+        d['reperes'][len(d['reperes'])-1]['numero']=l[num+5:]
+        state = 4
   elif state==4 and l[:7]=='Point :':
     d['reperes'][len(d['reperes'])-1]['indice']=l[8:]
     state = 5
@@ -57,12 +73,14 @@ for l in f:
     state=7
   elif (state==7 or state==6) and l[:27]=='Azimut de la prise de vue :':
     state=7
+  elif (state==7 or state==6) and l[:23]=='Compte-tenu des risques':
+    state=7
   elif (state==7 or state==6) and l[:23]=='Repère de nivellement :':
     d['reperes'][len(d['reperes'])-1]['nivellement']=l[24:]
   elif (state==7 or state==6) and l=='Exploitable directement par GPS':
     d['reperes'][len(d['reperes'])-1]['gps']='ok'
   elif state==6 and l!='':
-    d['reperes'][len(d['reperes'])-1]['description']=d['reperes'][len(d['reperes'])-1]['description']+', '+l
+    d['reperes'][len(d['reperes'])-1]['description']=d['reperes'][len(d['reperes'])-1]['description']+' '+l
   elif (state==6 or state==7) and l[:9]=='Système :': # début coordonnées
     # on récupère le SRS
     d['ref_latlon']=l[10:]
@@ -105,7 +123,12 @@ for l in f:
         else:
           c = re_alti.match(l)
           if c is not None:
-            d['reperes'][repere]['ele']=float(c.group(1))
+            try:
+                d['reperes'][repere]['ele']=float(c.group(1))
+            except:
+                if 'ele' not in d['reperes'][repere-1]:
+                    d['reperes'][repere-1]['ele']=float(c.group(1))
+                pass
           else:
             c = re_lonlat.match(l)
             if c is not None:
@@ -130,10 +153,15 @@ for l in f:
       d['ref_proj_epsg']='5490'
     if l.find('RGR92_07')>=0:
       d['ref_proj_epsg']='2975'
+    if l.find('RGFG95')>=0:
+      d['ref_proj_epsg']='4624'
     alti = l.find("Système altimétrique :")
     if alti>=0:
       d['ref_proj_alti']=l[alti+23:]
-   
+
+  elif state==10 and l=='(NGG) 1977':
+    d['ref_proj_alti'] = 'NGG 1977'
+
   elif state==10 and l=='e (m)':
     state=11
     repere = 0
@@ -160,7 +188,7 @@ for l in f:
     state=15
   elif state==15:
     if l.find('m')>0:
-      p = re.match(r".* (\d+)( |)(m|cm|mm)",l)
+      p = re.match(r".* (\d+)( |)(m|cm|mm)(| local)$",l)
       pr = int(p.group(1))
       if p.group(3)=='cm':
         pr = pr/100
@@ -172,7 +200,7 @@ for l in f:
         repere=0
       else:
         repere = repere+1
-      
+
   elif state==16 and l!='' and l.find('m')==-1:
     try: # au cas où il n'y a pas d'altitude pour le repère (exemple: 1905801.pdf)
       d['reperes'][repere]['z']=float(l)
@@ -188,7 +216,7 @@ for l in f:
     state=18
   elif state==18:
     if l.find('m')>0:
-      p = re.match(r".* (\d+)( |)(m|cm|mm)",l)
+      p = re.match(r".* (\d+)( |)(m|cm|mm)(| local)$",l)
       pr = int(p.group(1))
       if p.group(3)=='cm':
         pr = pr/100
@@ -199,6 +227,13 @@ for l in f:
       state=19
     else:
       repere = repere+1
-  
-print(json.dumps(d,sort_keys=True))
 
+if sortie=='json':
+    print(json.dumps(d,sort_keys=True))
+elif sortie == 'csv':
+    r = d['reperes']
+    del d['reperes']
+
+    for rep in r:
+        rep.update(d)
+        csvout.writerow(rep)
